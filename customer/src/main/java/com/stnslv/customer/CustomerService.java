390502b8e21f8ce4b522ad983ddc1dd10790ab01
Stanislav Ovsyankin <sovsyankin@arlo.com>
2022-03-08 01:43:28 +0600

    @LoadBalanced

--- customer/src/main/java/com/stnslv/customer/CustomerService.java
+++ customer/src/main/java/com/stnslv/customer/CustomerService.java
@@ -19,7 +19,7 @@ public class CustomerService {
                 .build();
         repository.saveAndFlush(customer);
         final FraudCheckResponse response = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
